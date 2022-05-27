bb1fa2bce2b1410a9a4b4e0d02303798cdd1b98d
Anjola <anjola.aina@gmail.com>
2022-05-26 19:42:51 -0600

    Created new instance variables in home page controller to start working on the table functionality

--- src/main/java/com/example/propertymanagementapplication/HomePageController.java
+++ src/main/java/com/example/propertymanagementapplication/HomePageController.java
@@ -2,7 +2,10 @@ package com.example.propertymanagementapplication;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.util.Date;
 
 public class HomePageController {
 
@@ -13,7 +16,31 @@ public class HomePageController {
     private MFXButton editButton;
 
     @FXML
-    private TableView table;
+    private TableView<Client> table;
+
+    @FXML
+    private TableColumn<Client, Date> dateJoinedColumn;
+
+    @FXML
+    private TableColumn<Client, String> clientNameColumn;
+
+    @FXML
+    private TableColumn<Client, String> tenantNameColumn;
+
+    @FXML
+    private TableColumn<Client, String> addressColumn;
+
+    @FXML
+    private TableColumn<Client, Double> rentColumn;
+
+    @FXML
+    private TableColumn<Client, Double> expensesColumn;
+
+    @FXML
+    private TableColumn<Client, Double> commissionColumn;
+
+    @FXML
+    private TableColumn<Client, Double> paymentColumn;
 
     /**
      * Initializes the home page;
